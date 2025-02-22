491d5f08ea9fc5ef24868a15115069449218ac30
PhamTuyen <phamquangtuyen001@gmail.com>
2025-02-22 08:35:38 +0700

    Conbo7

--- server.js
+++ server.js
@@ -4,27 +4,18 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
-
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Sử dụng routes
 app.use('/api', router);
-
-// Trang chủ
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-// Trang chatbot
 app.get('/chatbot', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'chatbot.html'));
 });
-
 app.listen(PORT, () => {
     console.log(`Server chạy tại http://localhost:${PORT}`);
 });
